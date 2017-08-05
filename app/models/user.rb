47ff1a9a1763100ae145e42e3a36225d492341c8
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-08-06 02:13:21 +0900

    user register with random thumbnail. But seed can't

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  
+  before_create :if_thumbnail_is_nil_thumbnail_is_random, on: :create
 
   has_many :post
   
@@ -6,4 +8,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  private
+  
+  def if_thumbnail_is_nil_thumbnail_is_random
+    if thumbnail == ''
+      self.thumbnail = "thumbnails/#{rand(0..9)}.png"
+    else 
+    end
+  end
 end
