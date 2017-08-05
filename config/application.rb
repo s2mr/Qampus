2457c8e459a56a7ee569c07a3f8b4070d4c3a856
Mitsuwo <hapowalp@gmail.com>
2017-08-05 14:02:28 +0900

    adopted bootstrap and added navbar

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,8 @@ Bundler.require(*Rails.groups)
 module Qampus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
+    config.time_zone = 'Tokyo' 
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
