ef779e493b962368b783c523bdb1aa0abe7cbf38
Mitsuwo <hapowalp@gmail.com>
2017-08-05 23:13:50 +0900

    added user show

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
 
+  get '/posts/users/:id' => 'users#show'
+
   root 'univs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
