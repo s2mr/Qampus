143088f559db345fabbc628638dd00a092994ffe
Kazumasa Shimomura <kazlv.1110@gmail.com>
2017-08-06 09:37:30 +0900

    fixed routes and design

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :posts
 
-  get '/posts/users/:id' => 'users#show'
+  get 'users/:id' => 'users#show', as: 'user' 
 
   root 'univs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
